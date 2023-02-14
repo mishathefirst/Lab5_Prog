3c7746abb0f09c8197454a2ebe37a9d4842a892b
wbadmin <mikhail.nogovitsyn2015@yandex.ru>
2023-02-14 23:58:17 +0300

    Locale created

--- src/com/lab5/UserInteraction.java
+++ src/com/lab5/UserInteraction.java
@@ -1,5 +1,7 @@
 package com.lab5;
 
+import java.util.Locale;
+import java.util.ResourceBundle;
 import java.util.Scanner;
 
 public class UserInteraction {
@@ -9,7 +11,9 @@ public class UserInteraction {
         CollectionManagement collectionManagement = new CollectionManagement();
 
         //TODO: errors processing
-        System.out.println("Type in the name of the file:");
+        //Locale locale = new Locale("en");
+        ResourceBundle r = ResourceBundle.getBundle("command_line");
+        System.out.println(r.getString("filegreeting"));
         Scanner in = new Scanner(System.in);
         String fileName = in.nextLine();
         collectionManagement.setCollection(fileProcessing.readFile(fileName));
