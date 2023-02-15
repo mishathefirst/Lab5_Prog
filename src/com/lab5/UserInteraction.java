466bfd7594d464de703ff5af29d30f9acb0f20b0
wbadmin <mikhail.nogovitsyn2015@yandex.ru>
2023-02-15 19:43:31 +0300

    User interaction on CollectionData implemented

--- src/com/lab5/UserInteraction.java
+++ src/com/lab5/UserInteraction.java
@@ -1,5 +1,7 @@
 package com.lab5;
 
+import com.lab5.business_entities.CollectionData;
+
 import java.util.Locale;
 import java.util.ResourceBundle;
 import java.util.Scanner;
@@ -9,6 +11,7 @@ public class UserInteraction {
     public void start() {
         FileProcessing fileProcessing = new FileProcessing();
         CollectionManagement collectionManagement = new CollectionManagement();
+
 
         //TODO: errors processing
         //Locale locale = new Locale("en");
@@ -22,10 +25,22 @@ public class UserInteraction {
         System.out.println("Type in the command:");
         String command = in.nextLine();
         while(!command.equals("exit")) {
-            System.out.println("Next String");
+            if (command.equals("info")) {
+                printCollectionInfo(collectionManagement.info());
+            }
+            System.out.println("Type in the command:");
             command = in.nextLine();
         }
         in.close();
+    }
+
+
+
+    private void printCollectionInfo(CollectionData collectionData) {
+        System.out.println("Тип коллекции: " + collectionData.getType());
+        System.out.println("Дата инициализации коллекции: " + collectionData.getInitialisationDate());
+        System.out.println("Размер коллекции: " + collectionData.getSize());
+        System.out.println("Пуста ли коллекция: " + collectionData.isEmpty());
     }
 
 }
--- src/com/lab5/business_entities/CollectionData.java
+++ src/com/lab5/business_entities/CollectionData.java
@@ -5,10 +5,10 @@ import java.util.HashSet;
 
 public class CollectionData {
 
-    String type;
-    long initialisationDate;
-    int size;
-    boolean isEmpty;
+    private String type;
+    private long initialisationDate;
+    private int size;
+    private boolean isEmpty;
 
     public CollectionData (String type, long initialisationDate, int size, boolean isEmpty) {
         this.type = type;
@@ -17,4 +17,35 @@ public class CollectionData {
         this.isEmpty = isEmpty;
     }
 
+    public String getType() {
+        return type;
+    }
+
+    public long getInitialisationDate() {
+        return initialisationDate;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return isEmpty;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public void setInitialisationDate(long initialisationDate) {
+        this.initialisationDate = initialisationDate;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public void setEmpty(boolean empty) {
+        isEmpty = empty;
+    }
 }
